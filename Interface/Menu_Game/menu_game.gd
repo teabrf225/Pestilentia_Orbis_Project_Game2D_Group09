80c225090d76ebb7db97f1716a70895006126e57
ArcAlter <pakawat2414@gmail.com>
2025-08-22 23:12:17 +0700

    Menu80

--- Interface/Menu_Game/menu_game.gd
+++ Interface/Menu_Game/menu_game.gd
@@ -1,17 +1,29 @@
 extends Control
 
+@onready var Main_buttons: VBoxContainer = $Main_buttons
+@onready var Options: Panel = $Options
+@onready var Credits: Panel = $Credits
+
+func _ready() -> void:
+	Main_buttons.visible = true
+	Options.visible = false
+	Credits.visible = false
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://World/Test/world_test.tscn")
 
 
 func _on_option_pressed() -> void:
-	pass # Replace with function body.
+	Options.visible = true
+
 
 
 func _on_credit_pressed() -> void:
-	pass # Replace with function body.
-
+	Credits.visible = true
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_back_options_pressed() -> void:
+	_ready()
