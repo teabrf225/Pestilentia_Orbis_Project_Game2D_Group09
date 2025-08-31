55bba99245fe045f566fe83228d0bdb10440f3f1
ArcAlter <pakawat2414@gmail.com>
2025-08-31 23:41:03 +0700

    loadDingSence_PauseMenu

--- Interface/Pause_menu/pause_menu.gd
+++ Interface/Pause_menu/pause_menu.gd
@@ -1,4 +1,6 @@
 extends Control
+@onready var Options: Panel = $Options
+@onready var Pause: Panel = $Panel
 
 func resume():
 	get_tree().paused = false
@@ -7,6 +9,7 @@ func resume():
 func pause():
 	get_tree().paused = true
 	show()
+	Options.visible = false
 	
 func _input(event):
 	if event.is_action_pressed("esc"):
@@ -19,7 +22,16 @@ func _on_resume_pressed() -> void:
 	resume()
 
 func _on_option_pressed() -> void:
-	pass # Replace with function body.
+	Options.visible = true
+	
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_back_pressed() -> void:
+	Options.visible = false
+
+
+func _on_pause_button_pressed() -> void:
+	pause()
