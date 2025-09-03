eb729a82633735a79e12c7256d48e3cc9f80f554
Phinatkanin <arzeus.pee2@gmail.com>
2025-09-03 23:58:27 +0700

    finish trap

---
+++ Resource/Trap/fire_trap.gd
@@ -0,0 +1,7 @@
+extends CharacterBody2D
+
+func _ready() -> void:
+	var b = randf_range(2,8)
+	await get_tree().create_timer(b).timeout
+	$AnimationPlayer.play("Fire")
+	$AnimatedSprite2D.play("Fire")
