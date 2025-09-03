2b949bde2e5b56d45bd7a8d258f7506f71855975
teabrf225 <teatar191@gmail.com>
2025-09-03 22:26:19 +0700

    Update State Idle, Wander

--- Management/States/Enemy/wander.gd
+++ Management/States/Enemy/wander.gd
@@ -3,12 +3,18 @@ extends State
 @export var enemy: Enemy
 @export var next_state: State
 var idle_timer: Timer
+@onready var direction_list = ["Left","Right"]
+var direction = null
 
 func enter():
 	super.enter()
-	enemy.velocity = Vector2.ZERO
+	var tmp = direction_list.pick_random()
+	#print(tmp)
+	direction = -1 if tmp == "Left" else 1
+	#print(direction)
+	enemy.update_facing_direction(tmp)
 	idle_timer = Timer.new()
-	idle_timer.wait_time = randi_range(3,10)
+	idle_timer.wait_time = randi_range(2,10)
 	idle_timer.timeout.connect(on_timeout)
 	idle_timer.autostart = true
 	add_child(idle_timer)
@@ -19,12 +25,17 @@ func exit():
 	idle_timer.timeout.disconnect(on_timeout)
 	idle_timer.queue_free()
 	idle_timer = null
+	direction = null
 
 # ฟังก์ชันที่ให้ Inherit เพื่อกำหนดพฤติกรรมของสถานะ
 # เช่น การเคลื่อนที่, การรับ input, การโจมตี
 # ทุกสถานะย่อยต้อง Override (เขียนโค้ดทับ) ฟังก์ชันนี้เพื่อทำงานของตัวเอง
 func update_state(_delta: float) -> void:
-	pass
+	if not enemy or direction == null:
+		return
+	else:
+		enemy.velocity = Vector2(direction*enemy.move_speed,enemy.velocity.y)
+		enemy.move_and_slide()
 
 # ฟังก์ชันที่ให้ Inherit เพื่อกำหนดเงื่อนไขการเปลี่ยนสถานะ
 # เช่น ตรวจสอบว่า Animation เล่นจบหรือผู้เล่นกดปุ่ม
@@ -33,5 +44,4 @@ func transition() -> void:
 	pass
 
 func on_timeout():
-	print("Time out idle")
 	request_change_to(next_state)
